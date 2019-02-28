59969dc8137db9f5d80e4707fead6d634e3ada5d
Anujin Munkhbat <anujin.munhbat@gmail.com>
2019-02-28 14:55:46 -0800

    home component and material module setup

---
+++ conference-frontend/src/app/material.module.ts
@@ -0,0 +1,36 @@
+import {
+  MatButtonModule,
+  MatAutocompleteModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatMenuModule,
+  MatSidenavModule,
+  MatListModule,
+  MatProgressSpinnerModule,
+  MatSnackBarModule, MatDialogModule, MatTooltipModule, MatCardModule, MatDividerModule, MatFormFieldModule, MatInputModule, MatSelectModule
+} from '@angular/material';
+import {NgModule} from '@angular/core';
+
+@NgModule({
+  exports: [
+    MatButtonModule,
+    MatAutocompleteModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatListModule,
+    MatProgressSpinnerModule,
+    MatSnackBarModule,
+    MatDialogModule,
+    MatTooltipModule,
+    MatCardModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule
+  ],
+})
+
+export class MaterialModule {}
